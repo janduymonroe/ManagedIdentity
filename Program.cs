83b962500b4f0fecec7f4d3d0b49d339394ebe9d
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-02-10 10:20:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,11 +15,8 @@ builder.Services.AddDbContext<ApplicationContext>(opt =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
