53126200c1898c4e0feab3a6ddafe46abf40f9f5
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-02-23 10:02:13 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationContext>(opt =>
 {
-    opt.UseSqlServer("Server=tcp:midb.database.windows.net,1433;Initial Catalog=ManagedIdentity;Persist Security Info=False;User ID=janduymonroe@wizsolucoes.com.br;Password=Suxmet13!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=\"Active Directory Password\";");
+    opt.UseSqlServer("Server=tcp:midb.database.windows.net,1433;Initial Catalog=ManagedIdentity;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=\"Active Directory Password\";");
 });
 
 var app = builder.Build();
